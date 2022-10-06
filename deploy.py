712d09cc87e87cbd2b051cc10bc523a06ab81fb4
Michael Akah <akahuzoma@gmail.com>
2022-10-06 17:02:49 +0000

    deployment to teh staging env

--- deploy.py
+++ deploy.py
@@ -1,3 +1,9 @@
 # Deploying a python web application
 echo "Hello World"   = shell scripts    
 print('Hello World') = python script
+deploy java application
+deploy python apps
+deploy in the dev env
+deployment5 master
+deployment4
+deploy to staging env
